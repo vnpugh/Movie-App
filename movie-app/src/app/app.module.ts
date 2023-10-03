9506c7a25c36f6a005bf27b92481922e35ff81e3
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-10-02 20:10:17 -0500

    Created shared components

--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MovieplatformComponent } from './movieplatform/movieplatform.component';
 import { MoviecollectionComponent } from './moviecollection/moviecollection.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { MovieComponent } from './shared/movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieplatformComponent,
-    MoviecollectionComponent
+    MoviecollectionComponent,
+    NavigationComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
